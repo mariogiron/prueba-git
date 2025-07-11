dcc563d09b2531dbccb8e0423c8b1245fdd80b5b
Marialafu <lafuentelhmaria@gmail.com>
2025-07-11 12:57:18 +0200

    cambios maria prueba 1

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -3,6 +3,11 @@ const router = require('express').Router();
 // TODO: Hacer las rutas de estudiantes
 console.log('Luego hago las rutas')
 
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
 // FIXME: arreglar la validación
 
 module.exports = router;
