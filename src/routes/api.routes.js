9076793eb4cf89ba6649d1b3a888573c6907fa8c
Mario Girón Martín <mariogironm@gmail.com>
2025-07-11 12:59:09 +0200

    Add debug variables and logs to API routes
    Introduced a temporary variable and additional console logs for debugging purposes in the API routes file. No functional changes to routing logic.

--- src/routes/api.routes.js
+++ src/routes/api.routes.js
@@ -1,8 +1,12 @@
 const router = require('express').Router();
 
+const cont = 0;
+
 // TODO: Hacer las rutas de estudiantes
 console.log('Luego hago las rutas')
 
 // FIXME: arreglar la validación
+
+console.log('lalala lelele');
 
 module.exports = router;
